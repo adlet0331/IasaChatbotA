597f1db3a0d6fa93d1c1c68cb96833434522d018
HyeongJu <eypk777@gmail.com>
2019-04-23 11:03:35 +0900

    GetMealJsonE

---
+++ Chatbot_server_meal/GetMealJson.js
@@ -0,0 +1,24 @@
+var moment = require('moment-timezone');
+var School = require('node-school-kr'); //today에 오늘의 급식이 들어가는게 원래 것인데 그걸 뺌. 상관 없긴 함
+var fs = require('fs');
+
+var CurrentDate = new Date();
+var kr = moment(CurrentDate).tz('Asia/Seoul');
+
+var year=kr.year();;
+var month=[kr.month()+1,kr.month()+2];
+
+console.log(year, month);
+
+const school = new School();
+school.init(School.Type.HIGH, School.Region.INCHEON, 'E100002238');
+
+var i; 
+for(i=0; i<month.length;i++){
+    school.getMeal(year, month[i]).then(meal => {
+        var txtname= meal.month + '.json';
+        var stringJson = JSON.stringify(meal);
+        fs.writeFileSync(txtname, stringJson, 'utf8');
+        console.log('File is saved at ' + txtname);
+    });
+}
